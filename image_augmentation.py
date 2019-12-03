7363c0cfab4e86a8619b9478897089d5473da92d
DSAureli <ds.aureli@gmail.com>
2019-12-03 09:08:36 +0100

    Move augmentation to Keras Sequence, add batch generation

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,72 +1,214 @@
-import os
 import random
 random.seed(1)
+
+import os
+import math
+import shutil
 
 import numpy as np
 import cv2
 import albumentations as alb
+from tensorflow.keras.utils import Sequence
 
 
-def generate_augm_set(root_path : str = r"./training",
-                      img_dir : str = r"images/",
-                      mask_dir : str = r"groundtruth/",
-                      augm : alb.core.composition.Compose = None,
-                      number : int = 1
-                      ) -> ((np.ndarray, np.ndarray), {str, int}):
-    
+class AugmentedSequence(Sequence):
+
     """
-    Generate image augmentation set.
+    Subclass of keras.utils.Sequence which generates the augmented image set.
 
-    Parameters
+    Attributes
     ----------
     root_path : string
-        The path of the directory which contains the image directory and the mask directory
+        The path of the directory which contains the image directory and the mask directory.
     img_dir : string
-        The path of the image directory relative to 'root_path'
+        The path of the image directory relative to 'root_path'.
     mask_dir : string
-        The path of the mask directory relative to 'root_path'
-    augm : albumentations Compose
-        Instance of alb.core.composition.Compose containing the transformations to perform
-    number : int
-        Number of augmentations generated per image
-
-    Returns
-    -------
-    ((image, mask), {"image", "num"}) : ((np.ndarray, np.ndarray), {str, int})
-        Tuple containing a tuple with image and mask and a dictionary with the image name and number.
-        If the image number is 0, the image returned is the original (non-augmented) image.
+        The path of the mask directory relative to 'root_path'.
+    img_size : int
+        The size of each generated image.
+        A random crop is performed on each image in the process of generation.
+    batch_size : int
+        The maximum size of each batch.
+        Note that if the mask of an image cannot be found, then the batch containing that image will be smaller,
+        as the number of batches must not change.
+    augm_count : int
+        Number of augmentations generated per image.
+        If less than 0, then the original images are returned without augmentation (except for cropping).
+        If greater than 0, each batch contains only the equivalent number of augmentations and no original image.
+    augm_compose : albumentations Compose
+        Instance of alb.core.composition.Compose containing the transformations to perform.
+        Note that a random crop of size img_size is always performed by the class prior to applying the provided composition.
     """
 
-    assert augm is not None, "Argument 'augm' cannot be None"
-    assert isinstance(augm, alb.core.composition.Compose), "Argument 'augm' must be instance of albumentations.core.composition.Compose"
+    def __init__(self,
+                 root_path: str = "./training",
+                 img_dir: str = "images",
+                 mask_dir: str = "groundtruth",
+                 img_size: int = 256,
+                 batch_size: int = 1,
+                 augm_count: int = 1,
+                 augm_compose: alb.core.composition.Compose = None
+                 ):
 
-    def meta_dict(img, n) -> dict:
-        return {"image": img, "num": n}
+        if augm_count < 1:
+            assert augm_compose is not None, "If argument 'augm_count' is less than 1, argument 'augm' shall not be None"
+            assert isinstance(augm_compose, alb.core.composition.Compose), \
+                "Argument 'augm' must be instance of albumentations.core.composition.Compose"
 
-    img_path = os.path.join(root_path, img_dir)
-    mask_path = os.path.join(root_path, mask_dir)
+        self.img_path = os.path.join(root_path, img_dir)
+        self.mask_path = os.path.join(root_path, mask_dir)
 
-    for img_file in sorted(os.listdir(img_path)):
-        img = cv2.imread(os.path.join(img_path, img_file))
-        mask = cv2.imread(os.path.join(mask_path, img_file))
+        self.img_files = sorted(os.listdir(self.img_path))
+        # self.mask_files = sorted(os.listdir(self.mask_path))
 
-        if mask is None:
-            print(f"== WARNING: Image {img_file} does not have corresponding mask in \"{mask_path}\", skipping. ==")
-            continue
+        # if self.img_files != self.mask_files:
+        #     diff = set(self.img_files) - set(self.mask_files)
+        #     raise Exception(f"The following training images do not have a corresponding mask in \"{mask_path}\":\n{diff}")
 
-        yield (img, mask), meta_dict(img_file, 0)
+        self.crop_compose = alb.Compose([alb.RandomCrop(width=img_size, height=img_size)])
+        # TODO closest size divisable by 32?
 
-        for idx in range(number):
-            augm_res = augm(image=img, mask=mask)
-            yield (augm_res["image"], augm_res["mask"]), meta_dict(img_file, idx+1)
+        self.batch_size = batch_size
+        self.augm_compose = augm_compose
+
+        # self.gen_count = 1 + augm_count
+        if augm_count < 1:
+            self.gen_count = 1
+            self.augm = False
+        else:
+            self.gen_count = augm_count
+            self.augm = True
+
+
+    def __len__(self):
+
+        """
+        Returns the number of batches
+        """
+
+        return math.ceil(len(self.img_files) * self.gen_count / self.batch_size)
+
+
+    def __getitem__(self, idx):
+
+        """
+        Returns one batch of generated images
+        """
+
+        def load_image_mask(idx):
+            img = cv2.imread(os.path.join(self.img_path, self.img_files[idx]))
+            mask = cv2.imread(os.path.join(self.mask_path, self.img_files[idx]))
+            return img, mask
+
+        # retrieve current image index and current augmentation index
+
+        images_done = idx * self.batch_size
+        batch_img_idx, batch_augm_idx = divmod(images_done, self.gen_count)
+
+        batch_img = []
+        batch_mask = []
+
+        img, mask = load_image_mask(batch_img_idx)
+        augm_space_iter = 0
+
+        # generate AT MOST self.batch_size images
+
+        while augm_space_iter < self.batch_size:
+
+            if batch_augm_idx < self.gen_count:
+
+                # there are still augmentations to generate for current image
+                # let's generate them
+
+                if mask is None:
+                    print(f"== WARNING: Image {self.img_files[batch_img_idx]}" +
+                          f"does not have corresponding mask in \"{self.mask_path}\"; skipping ==")
+
+                else:
+                    crop_res = self.crop_compose(image=img, mask=mask)
+                    img, mask = crop_res["image"], crop_res["mask"]
+
+                    if self.augm:
+                        augm_res = self.augm_compose(image=img, mask=mask)
+                        batch_img.append(augm_res["image"])
+                        batch_mask.append(augm_res["mask"])
+
+                    else:
+                        batch_img.append(img)
+                        batch_mask.append(mask)
+
+                batch_augm_idx += 1
+                augm_space_iter += 1
+
+            else:
+
+                # all augmentations for current images have been generated
+                # move to next image
+
+                batch_img_idx += 1
+                batch_augm_idx = 0
+
+                if batch_img_idx < len(self.img_files):
+                    img, mask = load_image_mask(batch_img_idx)
+                else:
+                    break
+
+        return np.array(batch_img), np.array(batch_mask)
+
+
+    def on_epoch_end(self):
+        pass
 
 
 if __name__ == "__main__":
 
-    img_size = 400
+    # test snippet for AugmentedSequence
+
+    ROOT_PATH = "./training"
+    IMG_DIR = "images"
+    MASK_DIR = "groundtruth"
+
+    TEST_PATH = "./augm_test"
+    AUGM_IMG_DIR = IMG_DIR + "_"
+    AUGM_MASK_DIR = MASK_DIR + "_"
+    TEST_COUNT = 10
+
+    if os.path.isdir(TEST_PATH):
+
+        while True:
+            regen = input(f"Test directory {TEST_PATH} already exists. Delete and regenerate before continuing? [y/n] ")
+
+            if regen == 'n':
+                break
+
+            elif regen == 'y':
+                shutil.rmtree(TEST_PATH)
+                break
+
+    test_augm_img_path = os.path.join(TEST_PATH, AUGM_IMG_DIR)
+    test_augm_mask_path = os.path.join(TEST_PATH, AUGM_MASK_DIR)
+
+    if not os.path.isdir(TEST_PATH):
+
+        test_img_path = os.path.join(TEST_PATH, IMG_DIR)
+        test_mask_path = os.path.join(TEST_PATH, MASK_DIR)
+
+        os.mkdir(TEST_PATH)
+        os.mkdir(test_img_path)
+        os.mkdir(test_mask_path)
+        os.mkdir(test_augm_img_path)
+        os.mkdir(test_augm_mask_path)
+
+        orig_img_path = os.path.join(ROOT_PATH, IMG_DIR)
+        orig_mask_path = os.path.join(ROOT_PATH, MASK_DIR)
+
+        for img_file in sorted(os.listdir(orig_img_path)) [:TEST_COUNT]:
+            shutil.copy2(os.path.join(orig_img_path, img_file), test_img_path)
+            shutil.copy2(os.path.join(orig_mask_path, img_file), test_mask_path)
+
+    IMG_SIZE = 400
 
     augm = alb.Compose([
-        alb.RandomCrop(width=img_size, height=img_size),
         alb.Flip(),
         alb.Transpose(),
         alb.RandomRotate90(),
@@ -91,20 +233,17 @@ if __name__ == "__main__":
         ]),
 
         alb.GaussNoise(p=0.25),
-        
         #alb.Normalize()
         ])
 
-    for (augm_img, augm_mask), meta_dict in generate_augm_set("augm_test", "image", "mask", augm=augm, number=10):
-        cv2.imwrite("augm_test/augm_image/augm_image_{}.png".format(meta_dict["num"]), augm_img)
-        cv2.imwrite("augm_test/augm_mask/augm_mask_{}.png".format(meta_dict["num"]), augm_mask)
+    for batch_idx, (augm_img_batch, augm_mask_batch) in \
+        enumerate(AugmentedSequence(root_path=TEST_PATH, img_size=IMG_SIZE, augm_compose=augm, augm_count=4, batch_size=5)):
 
-    # from tqdm import tqdm
-    # def test():
-    #    for _,_ in tqdm(generate_augm_set(augm=augm, number=10)):
-    #        pass
-    
-    # import timeit
-    # print(timeit.timeit(test, number=1))
+        print(f"{batch_idx}: {len(augm_img_batch)}")
+        print("---")
 
-    # 10 iterations per image (1000 generations + 100 originals) : 84s
+        for augm_img_idx, augm_img in enumerate(augm_img_batch):
+            cv2.imwrite(os.path.join(test_augm_img_path, f"batch_{batch_idx}-{augm_img_idx}.png"), augm_img)
+
+        for augm_mask_idx, augm_mask in enumerate(augm_mask_batch):
+            cv2.imwrite(os.path.join(test_augm_mask_path, f"batch_{batch_idx}-{augm_mask_idx}.png"), augm_mask)
